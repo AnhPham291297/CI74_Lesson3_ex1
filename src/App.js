6fddcae81e692955f081bfcdf97c1ba96f604ff0
AnhPham291297 <anh.potter1997@gmail.com>
2022-04-27 12:27:21 +0700

    lesson3_ex1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Button text="'Cancel'" />
+        <Button text="'Sell'" />
+        <Button text="'Set Auction'" />
+        <Button text="'Delist'" />
+        <Button text="'Approve'" />
+        <Button text="'Ok'" />
+        <Button text="'Create'" />
+        {/* 'Cancel','Sell','Set Auction','Delist','Approve','Ok',''Create" */}
+      </div>
     </div>
   );
 }
